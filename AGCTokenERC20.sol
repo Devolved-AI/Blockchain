f497d2146ff4b3762a481db0daa7eaba9cd9bf05
mitun567 <154320886+mitun567@users.noreply.github.com>
2023-12-21 11:08:59 +0600

    Update AGCTokenERC20.sol
    updating the name of erc20 token

--- AGCTokenERC20.sol
+++ AGCTokenERC20.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AGCToken is ERC20,Ownable {
-    constructor(address initialOwner) ERC20("Argo Coin", "AGC") Ownable(initialOwner) {
+    constructor(address initialOwner) ERC20("Argocoin", "AGC") Ownable(initialOwner) {
     _mint(msg.sender, 200000000 * 10**18); 
 }
 
